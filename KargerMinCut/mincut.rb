3bccafc086219ce1ccc4290d79972fa0364a64d4
Jon Seidel <jseidel@edpci.com>
2016-09-18 05:46:30 -0700

    Karger's MinCut...
    Basic cut routines work for small trees and graphs.

--- KargerMinCut/mincut.rb
+++ KargerMinCut/mincut.rb
@@ -1,16 +1,73 @@
 class MinCut
+
   def initialize(alist)
     fail "Adjacency List is not an array" if alist.class != Array
     fail "Adjacency list is too small" if alist.size <= 1
     @alist = alist
+    @nlist = []
+    @nlist = alist.map { |e| e[0] }
   end
+
+  def self.debug(debug = true)
+    $debug = debug
+  end
+
   def self.create_alist_from_file(fname)
     alist = []
+    nlist = []
     File.open(fname) do |f|
       f.readlines.each do |line|
-        alist << (line.chomp.split(',').map { |e| e.to_i })
+        nodelist = (line.chomp.split(',').map { |e| e.to_i })
+        alist << nodelist
+        nlist << nodelist[0]
       end
     end
-    alist
+    [nlist, alist]
+  end
+
+  def cut
+    # base case
+    if @alist.size == 2
+      return @alist[0].size - 1
+    end
+
+    puts "START: @nlist: #{@nlist}, @alist: #{@alist}" if $debug
+    # Select two random, connected nodes
+    nindex = rand(@nlist.size)
+    nitem = @nlist.at(nindex)
+    nconnect = @alist[nindex].clone
+    nconnect.delete_at 0      # Remove the node number
+    # Remember that we retain 'duplicate' edges to maintain the correct edge count
+    # So remove any duplicate retained edges so as not to bias the random selection
+    nconnect.uniq!
+    tindex = rand(nconnect.size)
+    citem = nconnect.at(tindex)
+    # puts "nconnect: #{nconnect} / tindex: #{tindex}, titem: #{titem}" if $debug
+    # It's OK that there are duplicated edges here; 'first' one is fine
+    cindex = @nlist.index(citem)
+    puts "Collapsing node #{citem} => #{nitem}"
+    # Collapse the two nodes into a single node
+    # Get all adjacent elements from both nodes
+    ta = @alist[nindex].slice(1, @alist.size-1) + @alist[cindex].slice(1, @alist.size-1)
+    puts "wip-adj/1: #{ta}" if $debug
+    # Delete any references to either node
+    ta.delete_if { |e| e == nitem || e == citem }
+    puts "wip-adj/2: #{ta}" if $debug
+    # Now put the remaining node number back in
+    ta.unshift nitem
+    puts "wip-adj/3: #{ta}" if $debug
+    # Delete the collapsed node from the node list and the adjacency list
+    @alist[nindex] = ta
+    @alist.delete_at cindex
+    @nlist.delete_at cindex
+    # Now replace all collapsed node numbers with the remaining node number
+    @alist.each_with_index do |e,i|
+      e.each_with_index do |n,j|
+        @alist[i][j] = nitem if @alist[i][j] == citem
+      end
+      # @alist[i].uniq!
+    end
+    puts "END: @nlist: #{@nlist}, @alist: #{@alist}" if $debug
+    cut
   end
 end
--- KargerMinCut/mincut_spec.rb
+++ KargerMinCut/mincut_spec.rb
@@ -12,17 +12,25 @@ create_test_file = ->(ary, fname='text.txt') {
 RSpec.describe MinCut do
   describe '::create_alist_from_file' do
     it 'returns a peroperly formatted aList' do
-      alist = MinCut.create_alist_from_file('test_one.txt')
+      nlist, alist = MinCut.create_alist_from_file('test_one.txt')
       expect(alist).to be_a Array
       expect(alist[0]).to be_a Array
+      expect(nlist).to be_a Array
+      expect(nlist.size).to eql alist.size
     end
     it 'reads in the simplest aList' do
-      alist = MinCut.create_alist_from_file('test_one.txt')
+      nlist, alist = MinCut.create_alist_from_file('test_one.txt')
       expect(alist).to eql [[1,2],[2,1]]
     end
     it 'the proc creates a simple test_file' do
       input = [ [1,2], [2,1]]
       create_test_file.call(input, 'test_one.txt')
+    end
+    it 'creates a more complicated test file' do
+      input = [ [1,2,3], [2,1,3], [3,1,2]]
+      create_test_file.call(input, 'test_two.txt')
+      nlist, alist = MinCut.create_alist_from_file('test_two.txt')
+      expect(input).to eql alist
     end
   end
   describe 'Basic setup' do
@@ -33,8 +41,36 @@ RSpec.describe MinCut do
     end
     it 'fails for too-small adjacency list' do
       expect{
-       MinCut.new [[1,2,3]]
+        MinCut.new [[1,2,3]]
       }.to raise_error('Adjacency list is too small')
+    end
+  end
+  describe 'handles simple graphs' do
+    before do
+      MinCut.debug
+    end
+    it 'cuts a 2-node graph' do
+      nlist, alist = MinCut.create_alist_from_file('test_one.txt')
+      mincut = MinCut.new(alist)
+      cut = mincut.cut
+      expect(cut).to eql 1
+    end
+    it 'cuts a 3-node graph' do
+      create_test_file.call([ [1,2,3], [2,1,3], [3,1,2] ], 'test.txt')
+      nlist, alist = MinCut.create_alist_from_file('test.txt')
+      puts "alist: #{alist}"
+      mincut = MinCut.new(alist)
+      cut = mincut.cut
+      expect(cut).to eql 2
+    end
+    it 'cuts a 3-node tree' do
+      MinCut.debug
+      create_test_file.call([ [1,4], [2,4], [3,4], [4,1,2,3] ], 'test.txt')
+      nlist, alist = MinCut.create_alist_from_file('test.txt')
+      puts "alist: #{alist}"
+      mincut = MinCut.new(alist)
+      cut = mincut.cut
+      expect(cut). to eql 1
     end
   end
 end
