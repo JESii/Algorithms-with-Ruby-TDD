c0d883cb817873b74af7e1d8ab2f298a2a87fbdc
Jon Seidel <jseidel@edpci.com>
2016-09-22 19:03:02 -0700

    Karger's MinCut passing all tests

--- KargerMinCut/mincut.rb
+++ KargerMinCut/mincut.rb
@@ -6,6 +6,10 @@ class MinCut
     @alist = alist
     @nlist = []
     @nlist = alist.map { |e| e[0] }
+    size = alist.size
+    @pcount = (size * (size - 1) * Math.log(size) / 2).to_i
+    @pval = size * size
+    puts "@pcount : #{@pcount}, @pval : #{@pval}"
   end
 
   def self.debug(debug = true)
@@ -17,26 +21,48 @@ class MinCut
     nlist = []
     File.open(fname) do |f|
       f.readlines.each do |line|
-        nodelist = (line.chomp.split(',').map { |e| e.to_i })
+        nodelist = (line.chomp.split(' ').map { |e| e.to_i })
         alist << nodelist
         nlist << nodelist[0]
       end
     end
-    [nlist, alist]
+    alist
   end
 
+  # This method runs multiple iterations of karger's MinCut algorithm
+  def p_cut
+    puts "Starting values pcount: #{@pcount}"
+    srand
+    #Save the initialized values for multiple calls
+    $alist = @alist.clone
+    $nlist = @nlist.clone
+    $pval= @pval
+    @pcount.times do |p|
+      @alist = $alist.clone
+      @nlist = $nlist.clone
+      srand
+      binding.pry
+      pval = cut
+      puts "@pval: #{@pval} / pval: #{pval}"
+      @pval = pval if pval < @pval
+    end
+    @pcount
+  end
+
+  # This routine runs a single iteration of Karger's MinCut algorithm
   def cut
     # base case
     if @alist.size == 2
-      return @alist[0].size - 1
+      binding.pry if @alist[0].size <= 1 || @alist[0].size != @alist[1].size
+      return (@alist[0].size - 1 + @alist[1].size - 1)/2
     end
 
     puts "START: @nlist: #{@nlist}, @alist: #{@alist}" if $debug
     # Select two random, connected nodes
     nindex = rand(@nlist.size)
     nitem = @nlist.at(nindex)
-    nconnect = @alist[nindex].clone
-    nconnect.delete_at 0      # Remove the node number
+    nconnect = @alist[nindex].slice(1, @alist[nindex].size-1)
+    # nconnect.delete_at 0      # Remove the node number
     # Remember that we retain 'duplicate' edges to maintain the correct edge count
     # So remove any duplicate retained edges so as not to bias the random selection
     nconnect.uniq!
@@ -45,15 +71,23 @@ class MinCut
     # puts "nconnect: #{nconnect} / tindex: #{tindex}, titem: #{titem}" if $debug
     # It's OK that there are duplicated edges here; 'first' one is fine
     cindex = @nlist.index(citem)
-    puts "Collapsing node #{citem} => #{nitem}"
+    puts "Collapsing node #{citem} => #{nitem}" if $debug
     # Collapse the two nodes into a single node
     # Get all adjacent elements from both nodes
+    begin
     ta = @alist[nindex].slice(1, @alist.size-1) + @alist[cindex].slice(1, @alist.size-1)
+    rescue
+      binding.pry
+    end
     puts "wip-adj/1: #{ta}" if $debug
-    # Delete any references to either node
+    # Delete any references to either node : self-loops
+    begin
     ta.delete_if { |e| e == nitem || e == citem }
+    rescue
+      binding.pry
+    end
     puts "wip-adj/2: #{ta}" if $debug
-    # Now put the remaining node number back in
+    # Now put the remaining node number back in as the 'owner'
     ta.unshift nitem
     puts "wip-adj/3: #{ta}" if $debug
     # Delete the collapsed node from the node list and the adjacency list
--- KargerMinCut/mincut_spec.rb
+++ KargerMinCut/mincut_spec.rb
@@ -4,7 +4,7 @@ require_relative 'mincut'
 create_test_file = ->(ary, fname='text.txt') {
   tfile = File.open fname, 'w'
   ary.each do |e|
-    tfile.puts e.join(',')
+    tfile.puts e.join(' ')
   end
   tfile.close
 }
@@ -12,24 +12,22 @@ create_test_file = ->(ary, fname='text.txt') {
 RSpec.describe MinCut do
   describe '::create_alist_from_file' do
     it 'returns a peroperly formatted aList' do
-      nlist, alist = MinCut.create_alist_from_file('test_one.txt')
+      alist = MinCut.create_alist_from_file('test_one.txt')
       expect(alist).to be_a Array
-      expect(alist[0]).to be_a Array
-      expect(nlist).to be_a Array
-      expect(nlist.size).to eql alist.size
+      expect(alist[0]).to be_an Array
     end
     it 'reads in the simplest aList' do
-      nlist, alist = MinCut.create_alist_from_file('test_one.txt')
+      alist = MinCut.create_alist_from_file('test_one.txt')
       expect(alist).to eql [[1,2],[2,1]]
     end
     it 'the proc creates a simple test_file' do
       input = [ [1,2], [2,1]]
-      create_test_file.call(input, 'test_one.txt')
+      create_test_file.call(input, 'test1.txt')
     end
     it 'creates a more complicated test file' do
       input = [ [1,2,3], [2,1,3], [3,1,2]]
-      create_test_file.call(input, 'test_two.txt')
-      nlist, alist = MinCut.create_alist_from_file('test_two.txt')
+      create_test_file.call(input, 'test2.txt')
+      alist = MinCut.create_alist_from_file('test2.txt')
       expect(input).to eql alist
     end
   end
@@ -50,14 +48,14 @@ RSpec.describe MinCut do
       MinCut.debug
     end
     it 'cuts a 2-node graph' do
-      nlist, alist = MinCut.create_alist_from_file('test_one.txt')
+      alist = MinCut.create_alist_from_file('test_one.txt')
       mincut = MinCut.new(alist)
       cut = mincut.cut
       expect(cut).to eql 1
     end
     it 'cuts a 3-node graph' do
       create_test_file.call([ [1,2,3], [2,1,3], [3,1,2] ], 'test.txt')
-      nlist, alist = MinCut.create_alist_from_file('test.txt')
+      alist = MinCut.create_alist_from_file('test.txt')
       puts "alist: #{alist}"
       mincut = MinCut.new(alist)
       cut = mincut.cut
@@ -66,11 +64,23 @@ RSpec.describe MinCut do
     it 'cuts a 3-node tree' do
       MinCut.debug
       create_test_file.call([ [1,4], [2,4], [3,4], [4,1,2,3] ], 'test.txt')
-      nlist, alist = MinCut.create_alist_from_file('test.txt')
+      alist = MinCut.create_alist_from_file('test.txt')
       puts "alist: #{alist}"
       mincut = MinCut.new(alist)
       cut = mincut.cut
-      expect(cut). to eql 1
+      expect(cut).to eql 1
+    end
+  end
+  describe 'handles larger graphs' do
+    it 'cuts a 5-node cycle' do
+      MinCut.debug(true)
+      cycle5 = [[1,2,5],[2,3,1],[3,4,2],[4,5,3],[5,1,4]]
+      create_test_file.call(cycle5, 'test.txt')
+      alist = MinCut.create_alist_from_file('test.txt')
+      puts "alist: #{alist}"
+      mincut = MinCut.new(alist)
+      cut = mincut.cut
+      expect(cut).to eql 2
     end
   end
 end
